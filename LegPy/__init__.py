3ad6576dd1ef943aa4d539d51a8f55e296f35fd7
F-Arqueros <arqueros@ucm.es>
2024-04-26 17:42:15 +0200

    Update __init__.py

--- LegPy/__init__.py
+++ LegPy/__init__.py
@@ -2,7 +2,7 @@
 
 from .MC import Plot_beam, MC
 from .geometry import Geometry
-from .medium import Medium, Material_Builder, List_Media
+from .medium import Medium, List_Media
 from .beam import Beam
 from .figures import *
 from .spectrum import Spectrum, List_Spectra
