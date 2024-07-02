5574c57ee61e7915ab8a43060325faad03630a30
F-Arqueros <arqueros@ucm.es>
2024-07-02 12:25:36 +0200

    isotropic non-point sources
    Fixed error in cylindrical source.
    
    Added other geometries of isotropic sources (orthohedron and sphere)

--- LegPy/beam.py
+++ LegPy/beam.py
@@ -3,7 +3,7 @@ import random
 import math
 
 #JR: Añado tipo de partícula particle = 'gamma' or 'electron'
-def Beam(name = 'parallel', diam = 0.0, length = 0., r_width = 0., x_ap = 0., y_ap = 0., theta = 0.0, phi = 0.0,
+def Beam(name = 'parallel', diam = 0.0, x_s = 0., y_s = 0., z_s = 0., r_s = 0., x_ap = 0., y_ap = 0., theta = 0.0, phi = 0.0,
          p_in = np.array ([0., 0., 0.]), particle = 'gamma'):
 
     theta, phi = math.radians(theta), math.radians(phi)
@@ -13,30 +13,53 @@ def Beam(name = 'parallel', diam = 0.0, length = 0., r_width = 0., x_ap = 0., y_
             raise ValueError('Please, input theta < 90 deg.')
         if diam==0.:
             beam = Pencil(theta, phi, p_in, particle)
-        else:      
+        else: # if 'parallel' diam is the beam diameter     
             beam = Broad(theta, phi, p_in, diam, particle)
 
     elif name == 'isotropic':
-        if diam==0.: 
-            if x_ap==0. and y_ap==0.:
-                beam = Isotropic_in(p_in, length, r_width, particle)
-        
-            else:
+        # if 'isotropic' diam is the diameter of a circular aperture of an external diverget beam
+        if diam==0.: # 'isotropic' without circular aperture (diam) 
+            if x_ap==0. and y_ap==0.: # without rectangular aperture 
+                # => Source inside the medium
+
+                if x_s * y_s * z_s != 0.: # Size in cartesian coordinates => Source is an orthohedron centered at p_in.  
+                    beam = Isotropic_in_ort(p_in, x_s, y_s, z_s, particle)
+                    if r_s != 0.:
+                        print('Warning: Source is an Orthohedron. Source radius is ignored') 
+                    
+                elif z_s * r_s != 0.: # Size in cylindrical coordinates => Cylindrical source 
+                    beam = Isotropic_in_cyl(p_in, z_s, r_s, particle)
+                    if x_s != 0. or y_s != 0.:
+                        print('Warning: Cylindrical source. x, y dimensions are ignored') 
+                    
+                elif z_s == 0. and r_s != 0.: # Only radius => Source is a sphere
+                    beam = Isotropic_in_sph(p_in, r_s, particle)
+                    if x_s != 0. or y_s != 0.:
+                        print('Warning: Spherical source. x, y dimensions are ignored')
+                
+                elif x_s == 0. and y_s == 0. and z_s == 0. and r_s == 0.: # Point source
+                    beam = Isotropic_in_p(p_in, particle)
+                    
+                else:
+                    raise ValueError('Please, input correct source size parameters')
+              
+                            
+            else: # source outside the medium with rectangular aperture
                 if p_in[2]<0.:
                     dist = abs(p_in[2])
-                    xy_in = p_in[0:-1] #xy coordinates of the source/focus
+                    xy_in = p_in[0:-1] #xy coordinates of the source/focus might be != (0,0)
                     xy_ap = np.array([[x_ap, y_ap],[-x_ap, y_ap],[x_ap, -y_ap],[-x_ap, -y_ap]]) # aperture size
                     XY = xy_in + xy_ap # coordinates of the aperture corners
                     diam_e = 2 * max(np.linalg.norm(XY[0]), np.linalg.norm(XY[1]),np.linalg.norm(XY[2]),np.linalg.norm(XY[3]))
                     cos_theta_max = 1. / (1. + (diam_e/2./dist)**2)**0.5
                     beam = Isotropic_out_r(dist, p_in, x_ap, y_ap, cos_theta_max, particle)
                 else:
-                    raise ValueError('Please, input valid source location')      
+                    raise ValueError('Please, input valid source location outside the medium')      
                            
-        else:
+        else: # divergent beam with circular aperture  
             if x_ap!=0. or y_ap!=0.: 
                 raise ValueError('Please, input valid aperture type')          
-            if p_in[2]<0.:            
+            if p_in[2]<0.:           
                 dist = abs(p_in[2])
                 diam_e = diam + 2.* math.sqrt(p_in[0]**2 + p_in[1]**2)
                 cos_theta_max = 1. / (1. + (diam_e/2./dist)**2)**0.5
@@ -116,18 +139,69 @@ class Isotropic_out_r:
                 i=1
                 return p_in_i, theta, phi
 
-class Isotropic_in:
-    def __init__(self, p_in, length, r_width, particle):
-        self.length = length
-        self.r_width = r_width
+class Isotropic_in_p: # point source
+    def __init__(self, p_in, particle):
         self.p_in = np.array(p_in)
         self.particle = particle
-
+        
     def in_track(self):
-        delta_z = (random.random() - 0.5) * self.length
-        delta_x = (random.random() - 0.5) * 2 * self.r_width
-        delta_y = (random.random() - 0.5) * 2 * self.r_width
+        theta = math.acos(1. - random.random() * 2.)
+        phi = 2. * math.pi * random.random() 
+        p_in = self.p_in
+        return p_in, theta, phi
+
+class Isotropic_in_ort: # orthohedro
+    def __init__(self, p_in, x_s, y_s, z_s, particle):
+        self.p_in = np.array(p_in)
+        self.particle = particle
+        self.x_s = x_s
+        self.y_s = y_s
+        self.z_s = z_s
+        
+    def in_track(self):
+        delta_x = (random.random() - 0.5) * self.x_s
+        delta_y = (random.random() - 0.5) * self.y_s
+        delta_z = (random.random() - 0.5) * self.z_s
         p_in_f = self.p_in + np.array([delta_x, delta_y, delta_z])
+        theta = math.acos(1. - random.random() * 2.)
+        phi = 2. * math.pi * random.random() 
+        return p_in_f, theta, phi
+
+class Isotropic_in_cyl: # cylindrical
+    def __init__(self, p_in, z_s, r_s, particle):
+        self.p_in = np.array(p_in)
+        self.particle = particle
+        self.r_s = r_s
+        self.z_s = z_s
+        
+    def in_track(self):
+        delta_z = (random.random() - 0.5) * self.z_s
+        i=0
+        while i==0:
+            delta_x = (random.random() - 0.5) * 2 * self.r_s
+            delta_y = (random.random() - 0.5) * 2 * self.r_s
+            if delta_x**2 + delta_y**2 <= self.r_s * self.r_s:
+                i = 1
+            p_in_f = self.p_in + np.array([delta_x, delta_y, delta_z])
+        theta = math.acos(1. - random.random() * 2.)
+        phi = 2. * math.pi * random.random() 
+        return p_in_f, theta, phi
+
+class Isotropic_in_sph: # sphere
+    def __init__(self, p_in, r_s, particle):
+        self.p_in = np.array(p_in)
+        self.particle = particle
+        self.r_s = r_s
+        
+    def in_track(self): 
+        i=0
+        while i==0:
+            delta_z = (random.random() - 0.5) * 2 * self.r_s
+            delta_x = (random.random() - 0.5) * 2 * self.r_s
+            delta_y = (random.random() - 0.5) * 2 * self.r_s
+            if delta_x**2 + delta_y**2 + delta_z**2 <= self.r_s * self.r_s:
+                i = 1
+            p_in_f = self.p_in + np.array([delta_x, delta_y, delta_z])
         theta = math.acos(1. - random.random() * 2.)
         phi = 2. * math.pi * random.random() 
         return p_in_f, theta, phi
